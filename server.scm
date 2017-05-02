720bc712eeac00416fa28ed903ebe1a41d164df9
Martin Ek <mail@ekmartin.com>
2017-05-02 11:31:12 -0400

    Add support for request handlers

--- server.scm
+++ server.scm
@@ -1,8 +1,15 @@
 (load "httpio.scm")
 
-(define HTTP/1.1 (cons 1 1))
-
+#|
+Initializes our web server.
+|#
 (define (create-server)
+  ;;; Local variables
+
+  ;;; (method path) -> handler
+  (define handlers (make-equal-hash-table))
+
+  ;;; Dispatchable procedures
   (define (listen tcp-port)
     (let ((socket (open-tcp-server-socket
 		   tcp-port
@@ -16,48 +23,81 @@
 		(dynamic-wind
 		    (lambda () unspecific)
 		    (lambda ()
-		      ;; TODO: handle errors and return 500 response:
 		      (handle-request (read-http-request port) port))
 		    (lambda ()
 		      (close-port port))))))
 	  (lambda () (channel-close socket)))))
 
+  (define (add-handler method path handler)
+    (let ((url (->uri path)))
+      (hash-table/put! handlers
+		       (cons method url)
+		       handler)))
+
+  ;;; Private helper procedures
+  (define (404-handler req)
+    '(404 () "Page Not Found"))
+
+  (define (wrap-handler handler)
+    (lambda (req)
+      ;; spread out the result with values,
+      ;; so we can use (receive) on the result:
+      (apply values (handler req))))
+
+  (define (get-handler method path)
+    (wrap-handler
+     (hash-table/get
+      handlers
+      (cons method path)
+      404-handler)))
+
+  (define (handle-request request port)
+    (let* ((path (http-request-uri request))
+	   (method (http-request-method request))
+	   (handler (get-handler method path)))
+      (receive
+       (status headers body)
+       (handler request)
+       (begin
+	 (write-http-response
+	  (make-http-response HTTP/1.1 status
+			      (http-status-description status)
+			      headers body)
+	  port)))))
+
   (define (dispatch op)
     (case op
       ((listen) listen)
+      ((add-handler) add-handler)
       (else (error "Unknown method: " op))))
 
   dispatch)
 
+(define HTTP/1.1 (cons 1 1))
+
 (define (listen server port)
   ((server 'listen) port))
 
-(define (handle-request request port)
-  (let* ((url (http-request-uri request))
-	 (path (uri-path url))
-	 ;; TODO: not sure why the first element
-	 ;; of the list is empty here:
-	 (filename (cadr path)))
-    (write-http-response
-     (make-http-response
-      HTTP/1.1 200
-      (http-status-description 200)
-      '()
-      ;; TODO: handle errors and return 404 response:
-      (read-file filename))
-     port)))
+(define (add-handler server method path handler)
+  ((server 'add-handler) method path handler))
 
 ;;; reads the string content at the given file path:
 (define (read-file filename)
   (list->string
    (let ((port (open-input-file filename)))
-    (let f ((x (read-char p)))
+    (let f ((x (read-char port)))
       (if (eof-object? x)
 	  (begin
-	    (close-input-port p)
+	    (close-input-port port)
 	    '())
-	  (cons x (f (read-char p))))))))
+	  (cons x (f (read-char port))))))))
 
 
 (define server (create-server))
+(add-handler server "GET" "/hello-world"
+	     (lambda (req) '(200 () "Hello World!")))
+
+(add-handler server "POST" "/cats"
+	     (lambda (req) '(200 () "\o/")))
+
 (listen server 3000)
