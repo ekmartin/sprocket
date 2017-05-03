8e050bf58d5a46b91a8879281abcef58690472d7
Martin Ek <mail@ekmartin.com>
2017-05-03 12:23:22 -0400

    Relicense to GPLv3, fixes #15
    Since we're modifying httpio.scm from mit-scheme, which uses GPLv2, we
    also need to use GPL.

--- httpio.scm
+++ httpio.scm
@@ -1,8 +1,8 @@
-(load-option '*PARSER)
-
-(declare (usual-integrations))
-
 #|
+This is a slightly modified version of runtime/httpio.scm in
+mit-scheme, to make read-http-request work with incoming TCP
+requests.
+
 Modified procedures
 parse-request-line:
 The original didn't support request lines that used
@@ -13,6 +13,10 @@ read-http-request:
 Didn't support empty bodies for some reason, which is well, all GET
 requests?
 |#
+
+(load-option '*PARSER)
+
+(declare (usual-integrations))
 
 (define parse-request-line
   (*parser
@@ -46,6 +50,38 @@ requests?
 		  (make-http-request method uri version
 				 (append! headers (cdr b.t))
 				 (car b.t)))))))))
+
+#| -*-Scheme-*-
+
+Copyright (C) 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994,
+    1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+    2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+    2017 Massachusetts Institute of Technology
+
+This file is part of MIT/GNU Scheme.
+
+MIT/GNU Scheme is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
+
+MIT/GNU Scheme is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MIT/GNU Scheme; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+|#
+
+;;;; HTTP I/O
+;;; package: (runtime http-i/o)
+
+;;; Assumptions:
+;;;   Transfer coding is assumed to always be "identity".
 
 
 (define-record-type <http-request>
